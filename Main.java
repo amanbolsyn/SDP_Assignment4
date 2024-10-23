67f8f87dc3f9725817439c484864c0d84fd6f8a3
Aman Nurgozhiyev <nurgozhiyevaman@gmail.com>
2024-10-23 14:24:03 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Example Usage
+public class Main {
+    public static void main(String[] args) {
+        OrderProcess orderProcess = new TaxiOrderProcess();
+        orderProcess.processOrder();
+    }
+}
